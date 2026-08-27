68c631c271d73186f5d7cab965280f02dae83e3f
tejovanth17 <tejovanth17@users.noreply.github.com>
2026-08-27 16:59:04 +0530

    Rename module to src to prevent shadowing Python standard library code module

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ root_dir = Path(__file__).resolve().parent
 if str(root_dir) not in sys.path:
     sys.path.insert(0, str(root_dir))
 
-from code.main import main
+from src.main import main
 
 if __name__ == "__main__":
     main()
